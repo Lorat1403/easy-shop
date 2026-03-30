7d912b85c49a47f7f61763c171f4e0ae0c9789d6
Lorat1403 <lorat1403@gmail.com>
2026-03-30 17:18:36 +0300

    created environment files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,23 +4,24 @@ import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBR7WPieyVoYU53fa-CVNNgR5LrpMN_g9I", 
-  authDomain: "easy-shop-6bb56.firebaseapp.com",
-  projectId: "easy-shop-6bb56",
-  storageBucket: "easy-shop-6bb56.firebasestorage.app",
-  messagingSenderId: "469362369701",
-  appId: "1:469362369701:web:9e08a4e54c9f9b8160994c"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBR7WPieyVoYU53fa-CVNNgR5LrpMN_g9I", 
+//   authDomain: "easy-shop-6bb56.firebaseapp.com",
+//   projectId: "easy-shop-6bb56",
+//   storageBucket: "easy-shop-6bb56.firebasestorage.app",
+//   messagingSenderId: "469362369701",
+//   appId: "1:469362369701:web:9e08a4e54c9f9b8160994c"
+// };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),    
     provideRouter(routes),
     
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyBR7WPieyVoYU53fa-CVNNgR5LrpMN_g9I", 
+  authDomain: "easy-shop-6bb56.firebaseapp.com",
+  projectId: "easy-shop-6bb56",
+  storageBucket: "easy-shop-6bb56.firebasestorage.app",
+  messagingSenderId: "469362369701",
+  appId: "1:469362369701:web:9e08a4e54c9f9b8160994c"
+  }
+};
